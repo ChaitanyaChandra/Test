f35570e6ed0b7865119d83f3ed9e1defd228cf5d
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2023-03-18 21:26:08 +0530

    automated commit #2062

---
+++ terraform/vpc.tf
@@ -0,0 +1,16 @@
+# aws_vpc --> predefined  resource name
+# vpc_one --> user defined resource name
+# Arguments --> parameters while creating the resource
+# Attributes --> outputs used to reference in other resources
+resource "aws_vpc" "vpc_one" {
+  cidr_block       = "10.0.0.0/16"  # perameters
+  instance_tenancy = "default"
+
+  tags = {
+    Name = var.vpc_name          # var reference
+  }
+}
+
+output "vpc_arn" {
+  value = aws_vpc.vpc_one.arn  # attribute
+}
