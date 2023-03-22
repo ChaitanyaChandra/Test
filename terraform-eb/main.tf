3656d29ded4731ed29e59ce5cfe7d77bd4b4e852
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2023-03-23 00:12:20 +0530

    automated commit #2074

--- terraform-eb/main.tf
+++ terraform-eb/main.tf
@@ -8,11 +8,11 @@ output "my_autoscaling_groups" {
   value = module.eb.auto_scaling_groups[0]
 }
 
-#resource "aws_autoscaling_lifecycle_hook" "lifecycle_hook" {
-#  count = length(module.eb.auto_scaling_groups)
-#  name                   = "lifecycle-hook-${count.index}"
-#  autoscaling_group_name = module.eb.auto_scaling_groups[count.index]
-#  default_result         = "CONTINUE"
-#  heartbeat_timeout      = 2000
-#  lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
-#}
+resource "aws_autoscaling_lifecycle_hook" "lifecycle_hook" {
+  count = length(module.eb.auto_scaling_groups)
+  name                   = "lifecycle-hook-${count.index}"
+  autoscaling_group_name = module.eb.auto_scaling_groups[count.index]
+  default_result         = "CONTINUE"
+  heartbeat_timeout      = 100
+  lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
+}
