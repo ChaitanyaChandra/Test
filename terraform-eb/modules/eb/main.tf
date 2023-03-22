afc4e85830b878b9a949acf8f48752eb778c2ce6
chaitanya chandra <chay@outlook.in>
2023-03-23 00:02:34 +0530

    automated commit #2071

--- terraform-eb/modules/eb/main.tf
+++ terraform-eb/modules/eb/main.tf
@@ -6,6 +6,13 @@ resource "aws_elastic_beanstalk_environment" "default" {
   name                = var.eb_enveronment_name
   application         = aws_elastic_beanstalk_application.default.name
   solution_stack_name = "64bit Amazon Linux 2 v3.5.0 running Python 3.8"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "arn:aws:iam::762942456166:instance-profile/EC2_admin"
+  }
+
 }
 
 output "auto_scaling_groups" {
