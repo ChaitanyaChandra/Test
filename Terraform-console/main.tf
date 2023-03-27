972368be3e8f70bce0c7cd40b7165a2f445a6d85
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2023-03-27 22:24:20 +0530

    automated commit #2079

--- Terraform-console/main.tf
+++ Terraform-console/main.tf
@@ -11,3 +11,23 @@ locals {
     }
   ]
 }
+
+resource "null_resource" "example" {
+  for_each = {
+    for env_name in local.env:
+    for eb in local.eb_env:
+    "${env_name}-${eb["name"]}" => {
+    env_name = env_name
+    eb_name = eb["name"]
+    }
+   }
+
+  triggers = {
+  env_name = each.value.env_name
+  eb_name = each.value.eb_name
+  }
+
+  provisioner "local-exec" {
+  command = "echo ${each.key}"
+  }
+}
