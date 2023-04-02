ad634b4194adb99031bdb1590ffe0004ea071e7e
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2023-04-02 06:02:21 +0530

    automated commit #2090

--- terraform/java/main.tf
+++ terraform/java/main.tf
@@ -1,13 +1,13 @@
 module "blue" {
   source = "../modules/subntes"
-  vpc_id = "vpc-004350fbaaccd6f6a"
+  vpc_id = "vpc-0aac120056603fad3"
   subnet_cider_range = "172.31.0.0/18"
   subnet_name = "blue-1"
 }
 
 module "green" {
   source = "../modules/subntes"
-  vpc_id = "vpc-004350fbaaccd6f6a"
+  vpc_id = "vpc-0aac120056603fad3"
   subnet_cider_range = "172.31.64.0/18"
   subnet_name = "green-1"
 }
