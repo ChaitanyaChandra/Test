af40a5a28dc5e52b183a7745c77d5b65c11dbf9a
chaitanya chandra <chay@outlook.in>
2023-04-02 08:00:57 +0530

    automated commit #2091

--- terraform/java/main.tf
+++ terraform/java/main.tf
@@ -1,13 +1,13 @@
 module "blue" {
   source = "../modules/subntes"
-  vpc_id = "vpc-0aac120056603fad3"
+  vpc_id = var.vpc_id
   subnet_cider_range = "172.31.0.0/18"
   subnet_name = "blue-1"
 }
 
 module "green" {
   source = "../modules/subntes"
-  vpc_id = "vpc-0aac120056603fad3"
+  vpc_id = var.vpc_id
   subnet_cider_range = "172.31.64.0/18"
   subnet_name = "green-1"
 }
