073960cd97b527a96393df9a43a1d435ba6f4168
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2023-05-05 18:38:59 +0530

    automated commit #2131

--- terraform-main/main.tf
+++ terraform-main/main.tf
@@ -1,10 +1,10 @@
-module "vpc" {
+module "vpc_one" {
   source = "../terraform-aws-vpc"
   vpc_name = "vpc-one"
   vpc_department = "direct source"
 }
 
-module "vpc" {
+module "vpc_two" {
   source = "github.com/ChaitanyaChandra/Test/terraform-aws-vpc"
   vpc_name = "vpc-two"
   vpc_department = "github source"
