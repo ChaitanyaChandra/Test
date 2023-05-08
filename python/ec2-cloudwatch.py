af2892f2a5ed5b681501e9d24bc74a6af4aae3d0
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2023-05-08 22:35:21 +0530

    automated commit #2181

--- python/ec2-cloudwatch.py
+++ python/ec2-cloudwatch.py
@@ -69,8 +69,9 @@ def calculate(days):
                 print(f"No data available for the past {days} days.")
 
 
+calculate(5)
 calculate(10)
-calculate(20)
+calculate(15)
 keys = instance_data[0].keys()
 # Write the instance data to a CSV file
 with open('instance_data.csv', 'w', newline='') as output_file:
