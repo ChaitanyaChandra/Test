4462b3c5d2292a7669af3890cb6bb3256822090b
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2023-05-08 11:13:30 +0530

    automated commit #2154

--- python/ec2-cloudwatch.py
+++ python/ec2-cloudwatch.py
@@ -57,7 +57,7 @@ for data in instance_data:
     datapoints = response['Datapoints']
     if len(datapoints) > 0:
         average_cpu = datapoints[-1]['Average']
-        print(average_cpu)
+        print(datapoints[-1])
         print(f"{data.get('InstanceId')} Average CPU utilization over the past 30 days: {average_cpu:.2f}%")
     else:
         print("No data available for the past 30 days.")
