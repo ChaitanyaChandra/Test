446eab5101fd954ed147101201f398f4b5ff98e7
chaitanya chandra <chay@outlook.in>
2023-05-08 12:52:13 +0530

    automated commit #2173

--- python/ec2-cloudwatch.py
+++ python/ec2-cloudwatch.py
@@ -74,8 +74,8 @@ with open('instance_data.csv', mode='w', newline='') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     for data in instance_data:
-        for k, v in data:
-            print(k,v)
+        for k in data:
+            print(k)
             #writer.writerow(data)
 
 print(instance_data)
