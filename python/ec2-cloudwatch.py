42775cc087da214490570d2ae1bda5905e7b6fcf
chaitanya chandra <chay@outlook.in>
2023-05-08 22:54:53 +0530

    automated commit #2195

--- python/ec2-cloudwatch.py
+++ python/ec2-cloudwatch.py
@@ -40,7 +40,6 @@ def calculate(days):
     for metric in metrics:
         for k, v in metric.items():
             for data in instance_data:
-                print(f"k and v is {k, v}")
                 response = cloudwatch.get_metric_statistics(
                     Namespace='AWS/EC2',
                     MetricName=k,
@@ -59,19 +58,19 @@ def calculate(days):
 
                 datapoints = response['Datapoints']
                 print(response)
-                # metric_datapoints = []
-                # if len(datapoints) > 0:
-                #     for datapoint in datapoints:
-                #         metric_datapoints.append(datapoint.get('Average'))
-                #     # print(average_cpu)
-                #     average_metric = sum(metric_datapoints) / len(metric_datapoints)
-                #     data.update({f"Average_{k}_past_{days}_days" : average_metric})
-                #     # print(f"{data.get('InstanceId')} Average CPU utilization over the past {days} days: {average_cpu:.2f}%")
-                # else:
-                #     print(f"No data available for the past {days} days.")
+                metric_datapoints = []
+                if len(datapoints) > 0:
+                    for datapoint in datapoints:
+                        metric_datapoints.append(datapoint.get('Average'))
+                    # print(average_cpu)
+                    average_metric = sum(metric_datapoints) / len(metric_datapoints)
+                    data.update({f"Average_{k}_past_{days}_days" : average_metric})
+                    # print(f"{data.get('InstanceId')} Average CPU utilization over the past {days} days: {average_cpu:.2f}%")
+                else:
+                    print(f"No data available for the past {days} days.")
 
 
-calculate(5)
+calculate(instance_data)
 # calculate(10)
 # calculate(15)
 # keys = instance_data[0].keys()
