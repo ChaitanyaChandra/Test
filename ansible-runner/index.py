6c484425932e98c1fc692ab2473db74480a434fe
ChaitanyaChandra <chay@outlook.in>
2024-09-04 15:34:05 +0530

    automated commit #2774

--- ansible-runner/index.py
+++ ansible-runner/index.py
@@ -1,10 +1,18 @@
 import ansible_runner
-r = ansible_runner.run(private_data_dir='./', inventory="/Users/chandra/Documents/Test/ansible-runner/inv.ini", playbook='ansible-playbooks/main.yml', extravars={"my_hosts": "nodes"})
-inventory_hosts = ["node1.chaitu.net", "node2.chaitu.net"]  # Replace 'all' with your group if needed
-for host in inventory_hosts:
-    facts = r.get_fact_cache(host)
-    print(f"Facts for {host}:")
-    playbook_message = facts.get('playbook_message', 'insync')
-    print(playbook_message)
+import pandas as pd
 
-print(r.status)
+r = ansible_runner.run(private_data_dir='./', inventory="/Users/chandra/Documents/Test/ansible-runner/inv.ini", playbook='ansible-playbooks/main.yml', extravars={"my_hosts": "nodes"})
+nodes = ["node1.chaitu.net", "node2.chaitu.net", "node3.chaitu.net", "node5.chaitu.net"]
+df = pd.DataFrame({f"nodes": nodes})
+data={}
+for host in nodes:
+    facts = r.get_fact_cache(host)
+    # print(f"Facts for {host}:")
+    response = facts.get('response', 'unreachible')
+    # print(response)
+    data[host]=response
+
+df['check version'] = df['nodes'].map(data)
+
+# print(df)
+df.to_csv("hello.csv", index=False)
