6e7c83fa0e63918bd356d341b3bf6ca9344a0184
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2024-10-06 12:46:35 +0530

    automated commit #2808

--- ansible-runner/index.py
+++ ansible-runner/index.py
@@ -1,18 +1,18 @@
 import ansible_runner
 import pandas as pd
+import os
 
-r = ansible_runner.run(private_data_dir='./', inventory="/Users/chandra/Documents/Test/ansible-runner/inv.ini", playbook='ansible-playbooks/main.yml', extravars={"my_hosts": "nodes"}, quiet=True)
-nodes = ["node1.chaitu.net", "node2.chaitu.net", "node3.chaitu.net", "node5.chaitu.net"]
-df = pd.DataFrame({f"nodes": nodes})
-data={}
-for host in nodes:
-    facts = r.get_fact_cache(host)
-    # print(f"Facts for {host}:")
-    response = facts.get('response', 'unreachible')
-    # print(response)
-    data[host]=response
+pwd = os.getcwd()
 
-df['check version'] = df['nodes'].map(data)
+r = ansible_runner.run(private_data_dir='./', inventory=f"{pwd}/ansible-runner/inv.ini", playbook='ansible-playbooks/async.yml', extravars={"my_hosts": "nodes"}, quiet=False)
 
-print(df)
-df.to_csv("hello.csv", index=False)
+# facts = r.get_fact_cache("localhost")
+# # print(f"Facts for {host}:")
+# response = facts.get('response', 'unreachible')
+# # print(response)
+# data[host]=response
+#
+# df['check version'] = df['nodes'].map(data)
+#
+# print(df)
+# df.to_csv("hello.csv", index=False)
