f3ce6ea18f0ef880e6d711e224ecebb8b31ae0d1
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2025-05-07 13:57:56 +0530

    Automated Commit #3031

--- python/sql_table_to_csv.py
+++ python/sql_table_to_csv.py
@@ -2,11 +2,11 @@ import mysql.connector
 import pandas as pd
 
 conn = mysql.connector.connect(
-    host="",
-    user="",
-    password="",
-    database=""
+    host="localhost",
+    user="your_user",
+    password="your_pass",
+    database="your_db"
 )
-df = read_sql("SELECT * FROM test_table")
+df = pd.read_sql("SELECT * FROM test_table", conn)
 df.to_csv("output.csv", index=False)
 conn.close()
