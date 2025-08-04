76c1fe1d7dab1bda22eba404a6dd867ed5ca2e67
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2025-08-04 15:49:10 +0530

    Automated Commit #3060

--- docker/script.sh
+++ docker/script.sh
@@ -5,3 +5,10 @@ docker push docker.io/chaitanyachandra/awx-operator:latest
 docker build -t docker.io/chaitanyachandra/kube-rbac-proxy:v0.15.0  -t docker.io/chaitanyachandra/kube-rbac-proxy:latest .
 docker push docker.io/chaitanyachandra/kube-rbac-proxy:v0.15.0
 docker push docker.io/chaitanyachandra/kube-rbac-proxy:latest
+
+kubectl create secret docker-registry regcred \
+  --docker-server=https://index.docker.io/v1/ \
+  --docker-username=<your-dockerhub-username> \
+  --docker-password=<your-dockerhub-password> \
+  --docker-email=<your-email> \
+  -n awx
