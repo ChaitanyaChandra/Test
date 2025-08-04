df5535d69420fce817eb3730cd88201018c48145
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2025-08-04 15:09:42 +0530

    Automated Commit #3059

--- docker/script.sh
+++ docker/script.sh
@@ -1,3 +1,7 @@
 docker build -t docker.io/chaitanyachandra/awx-operator:2.19.1 -t docker.io/chaitanyachandra/awx-operator:latest .
 docker push docker.io/chaitanyachandra/awx-operator:2.19.1
 docker push docker.io/chaitanyachandra/awx-operator:latest
+
+docker build -t docker.io/chaitanyachandra/kube-rbac-proxy:v0.15.0  -t docker.io/chaitanyachandra/kube-rbac-proxy:latest .
+docker push docker.io/chaitanyachandra/kube-rbac-proxy:v0.15.0
+docker push docker.io/chaitanyachandra/kube-rbac-proxy:latest
