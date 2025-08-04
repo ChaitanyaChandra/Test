185316be022dbc99360b9c8dcb3d2b97874f4191
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2025-08-04 14:47:09 +0530

    Automated Commit #3055

---
+++ docker/script.sh
@@ -0,0 +1,3 @@
+docker build -t docker.io/chaitanyachandra/awx-operator:2.19.1 -t docker.io/chaituchowdary/awx-operator:latest .
+docker push docker.io/chaitanyachandra/awx-operator:2.19.1
+docker push docker.io/chaituchowdary/awx-operator:latest
