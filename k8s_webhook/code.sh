3c5b34bc30ff16db984afdd7868989520fb35136
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2025-08-12 17:30:34 +0530

    Automated Commit #3086

--- k8s_webhook/code.sh
+++ k8s_webhook/code.sh
@@ -1,11 +1,13 @@
 # Generate private key
 openssl genrsa -out webhook-server.key 2048
 
+# https://webhook-service.webhook.svc:443/mutate?timeout=5s
+
 # Generate a CSR
-openssl req -new -key webhook-server.key -subj "/CN=webhook-app.webhook.svc.svc.durgasri.in" -out webhook-server.csr
+openssl req -new -key webhook-server.key -subj "/CN=webhook-service.webhook.svc" -out webhook-server.csr
 
 # Generate a self-signed CA
-openssl req -x509 -newkey rsa:4096 -sha256 -days 10000 -nodes -keyout ca.key -out ca.crt -subj "/CN=webhook-app.webhook.svc.svc.durgasri.in"
+openssl req -x509 -newkey rsa:4096 -sha256 -days 10000 -nodes -keyout ca.key -out ca.crt -subj "/CN=webhook-service.webhook.svc"
 
 # Sign the webhook server CSR with the CA
 openssl x509 -req -in webhook-server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out webhook-server.crt -days 10000 -sha256
@@ -16,6 +18,8 @@ mv webhook-server.key tls.key
 
 # Encode CA cert for kubernetes webhook YAML
 cat ca.crt | base64 | tr -d '\n' > ca_bundle.txt
+
+kubectl create secret tls webhook-tls-secret --cert=tls.crt --key=tls.key -n webhook
 
 docker build -t docker.io/chaitanyachandra/webhook:arm_1.0  -t docker.io/chaitanyachandra/webhook:arm_latest .
 docker push docker.io/chaitanyachandra/webhook:arm_1.0
