ee79de1c341278e3085f563c1a7d0c670a0d9c79
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2025-08-12 09:34:42 +0530

    Automated Commit #3075

--- docker/script.sh
+++ docker/script.sh
@@ -15,6 +15,25 @@ docker build -t docker.io/chaitanyachandra/app:amd_1.0  -t docker.io/chaitanyach
 docker push docker.io/chaitanyachandra/app:amd_1.0
 docker push docker.io/chaitanyachandra/app:amd_latest
 
+# multi tag arch
+docker manifest create docker.io/chaitanyachandra/app:1.0 \
+  --amend docker.io/chaitanyachandra/app:arm_1.0 \
+  --amend docker.io/chaitanyachandra/app:amd_1.0
+
+docker manifest push docker.io/chaitanyachandra/app:1.0
+
+docker manifest create docker.io/chaitanyachandra/app:latest \
+  --amend docker.io/chaitanyachandra/app:arm_latest \
+  --amend docker.io/chaitanyachandra/app:amd_latest
+
+docker manifest push docker.io/chaitanyachandra/app:latest
+
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t docker.io/chaitanyachandra/app:1.0 \
+  -t docker.io/chaitanyachandra/app:latest \
+  --push .
+
+
 kubectl create secret docker-registry regcred \
   --docker-server=https://index.docker.io/v1/ \
   --docker-username=<your-dockerhub-username> \
