d21ae872a1ef341bf266c52d799eb19f20039d61
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2025-10-26 07:45:02 +0530

    Automated Commit #3234

--- k8s_webhook/code.sh
+++ k8s_webhook/code.sh
@@ -7,11 +7,35 @@ openssl genrsa -out webhook-server.key 2048
 openssl req -new -key webhook-server.key -subj "/CN=webhook-service.webhook.svc" -out webhook-server.csr
 
 # Generate a self-signed CA
-openssl req -x509 -newkey rsa:4096 -sha256 -days 10000 -nodes -keyout ca.key -out ca.crt -subj "/CN=webhook-service.webhook.svc"
+openssl req -x509 -newkey rsa:4096 -sha256 -days 10000 -nodes \
+  -keyout ca.key -out ca.crt -subj "/CN=webhook-ca"
+
+# Create a CSR config file for SAN (Subject Alternative Names)
+cat <<EOF > csr.conf
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+distinguished_name = dn
+req_extensions = v3_req
+
+[ dn ]
+CN = webhook-service.webhook.svc
+
+[ v3_req ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = webhook-service
+DNS.2 = webhook-service.webhook
+DNS.3 = webhook-service.webhook.svc
+EOF
 
 # Sign the webhook server CSR with the CA
-openssl x509 -req -in webhook-server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out webhook-server.crt -days 10000 -sha256 -extensions v3_req -extfile csr.conf
-
+openssl x509 -req -in webhook-server.csr \
+  -CA ca.crt -CAkey ca.key -CAcreateserial \
+  -out webhook-server.crt -days 10000 -sha256 \
+  -extensions v3_req -extfile csr.conf
 
 # mv cert and key as tls.crt and tls.key
 mv webhook-server.crt tls.crt
@@ -20,7 +44,11 @@ mv webhook-server.key tls.key
 # Encode CA cert for kubernetes webhook YAML
 cat ca.crt | base64 | tr -d '\n' > ca_bundle.txt
 
-kubectl create secret tls webhook-tls-secret --cert=tls.crt --key=tls.key -n webhook
+
+kubectl create secret tls webhook-tls-secret \
+  --cert=tls.crt \
+  --key=tls.key \
+  -n webhook
 
 docker build -t docker.io/chaitanyachandra/webhook:arm_2.0  -t docker.io/chaitanyachandra/webhook:arm_latest .
 docker push docker.io/chaitanyachandra/webhook:arm_2.0
