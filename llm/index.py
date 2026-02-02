f6f9f364191ca6f8853602b46d0bdd157ebe2823
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2026-02-02 14:40:24 +0530

    Automated Commit #3350

--- llm/index.py
+++ llm/index.py
@@ -1,29 +1,14 @@
-def list_configmaps(self, namespace: str, cluster: str) -> List[str]:
-    """List configmap names in a Kubernetes namespace"""
+def list_ingresses(self, namespace: str, cluster: str) -> List[dict]:
+    """List ingress names in a Kubernetes namespace"""
     self._require_ns(namespace)
-    config_maps = []
-    core_v1_api: client.CoreV1Api = self.config_map.get_config_details(cluster).k8s_core_v1_api
-    for cm in core_v1_api.list_namespaced_config_map(namespace).items:
-        config_maps.append({
-            "name": cm.metadata.name,
-            "data": cm.data,
-            "created_at": cm.metadata.creation_timestamp,
+    net_v1_api: client.NetworkingV1Api = self.config_map.get_config_details(cluster).k8s_networking_v1_api
+    ingress = []
+    for ing in net_v1_api.list_namespaced_ingress(namespace).items:
+        ingress.append({
+            "name": ing.metadata.name,
+            "ingress_class_name": ing.spec.ingress_class_name,
+            "rules": ing.spec.rules,
+            "created_at": ing.metadata.creation_timestamp,
         })
-    logger.info(f"Fetched configmaps list {[cm['name'] for cm in config_maps]} in namespace {namespace}")
-    return config_maps
-
-
-def list_secrets(self, namespace: str, cluster: str) -> List[dict]:
-    """List all secrets in a Kubernetes namespace"""
-    self._require_ns(namespace)
-    secrets = []
-    core_v1_api: client.CoreV1Api = self.config_map.get_config_details(cluster).k8s_core_v1_api
-    for secret in core_v1_api.list_namespaced_secret(namespace).items:
-        secrets.append({
-            "name": secret.metadata.name,
-            "type": secret.type,
-            "data": secret.data,
-            "created_at": secret.metadata.creation_timestamp,
-        })
-    logger.info(f"Fetched secrets list {[s['name'] for s in secrets]} in namespace {namespace}")
-    return secrets
+    logger.info(f"Fetched ingresses list {[ing['name'] for ing in ingress]} in namespace {namespace}")
+    return ingress
