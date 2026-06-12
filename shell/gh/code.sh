f3a7616ba25991c0862ca31705fba1114bc934c8
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2026-06-12 15:00:26 +0530

    Automated Commit #

--- shell/gh/code.sh
+++ shell/gh/code.sh
@@ -5,7 +5,7 @@ summary=$'# STATUS'
 summary+=$'\n| Item | Data | Status |'
 summary+=$'\n|-----|------|--------|'
 
-time="date '+%Y-%m-%d %H:%M:%S'"
+time=$(date '+%Y-%m-%d %H:%M:%S')
 summary+="\n| Timestamp | '$time' | Success |"
 
 echo -e "$summary" >> $GITHUB_STEP_SUMMARY
