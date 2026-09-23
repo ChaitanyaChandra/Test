2b0e09a880440528e8ceaf479e098be934685410
ChaitanyaChandra <ChaitanyaChandra@icloud.com>
2026-09-23 20:25:59 +0530

    Automated Commit #

--- webhook/app/index.py
+++ webhook/app/index.py
@@ -4,14 +4,16 @@ from routers import validate_rewrite, requests_limits
 import logging
 
 
-if os.getenv("DEBUG", "true").lower()  == "true":
+if os.getenv("DEBUG", "true").lower() == "true":
     logging.root.setLevel(logging.DEBUG)
+
 
 app = FastAPI()
 
 app.include_router(validate_rewrite.router)
 app.include_router(requests_limits.router)
 
-app.get("/health")
+
+@app.get("/health")
 def health():
     return "200 - OK"
